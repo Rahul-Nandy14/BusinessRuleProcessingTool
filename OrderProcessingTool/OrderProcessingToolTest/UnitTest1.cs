e6219f6f3605d1b28a6647ba67e4f5f0a8523d7a
Rahulatheist <65806141+Rahulatheist@users.noreply.github.com>
2020-07-10 21:27:48 +0530

    test code for upgrade

--- OrderProcessingTool/OrderProcessingToolTest/UnitTest1.cs
+++ OrderProcessingTool/OrderProcessingToolTest/UnitTest1.cs
@@ -9,14 +9,6 @@ namespace OrderProcessingToolTest
         [TestMethod]
         public void ShouldReturnVideoSlipOnlyFromOderProcessingUnitTest()
         {
-            Video v = new Video("abcd");
-            Assert.AreEqual("abcd", v.ItemName);
-            Assert.AreEqual("Generated a packing slip.", v.Operations[0]);
-        }
-
-        [TestMethod]
-        public void ShouldReturnVideoLearningToSkiSlipOnly()
-        {
             var result = OrderProcessor.ConvertInputToType(new string[] { "video", "Random" });
             Assert.AreEqual("Random", result.ItemName);
             Assert.AreEqual("Generated a packing slip.", result.Operations[0]);
@@ -24,13 +16,35 @@ namespace OrderProcessingToolTest
         }
 
         [TestMethod]
-        public void ShouldReturnMembershipSlip()
+        public void ShouldReturnVideoLearningToSkiSlipOnly()
         {
             var result = OrderProcessor.ConvertInputToType(new string[] { "video", "Learning To Ski" });
             Assert.AreEqual("Learning To Ski", result.ItemName);
             Assert.AreEqual("Generated a packing slip.", result.Operations[0]);
             Assert.AreEqual("'First Aid' video added to the packing slip", result.Operations[1]);
             Assert.AreEqual(2, result.Operations.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnMembershipSlip()
+        {
+            var result = OrderProcessor.ConvertInputToType(new string[] { "Upgrade", "Random" });
+            Assert.IsNull(result.ItemName);
+            Assert.AreEqual("Generated a packing slip.", result.Operations[0]);
+            Assert.AreEqual("Apply the upgrade", result.Operations[1]);
+            Assert.AreEqual("Mail Sent", result.Operations[2]);
+            Assert.AreEqual(3, result.Operations.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnUpgradeSlipOnly()
+        {
+            var result = OrderProcessor.ConvertInputToType(new string[] { "Upgrade", "Random" });
+            Assert.IsNull(result.ItemName);
+            Assert.AreEqual("Generated a packing slip.", result.Operations[0]);
+            Assert.AreEqual("Apply the upgrade", result.Operations[1]);
+            Assert.AreEqual("Mail Sent", result.Operations[2]);
+            Assert.AreEqual(3, result.Operations.Count);
 
         }
     }
